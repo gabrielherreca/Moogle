1cc6efe44db0f6a1aa95ebfda6587ec780eeeee1
gabrielherreca <132971921+gabrielherreca@users.noreply.github.com>
2023-05-11 23:48:08 -0700

    Update Moogle.cs
    Changing functions names

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -1,4 +1,4 @@
-﻿namespace MoogleEngine;
+namespace MoogleEngine;
 
 
 public static class Moogle
@@ -7,13 +7,13 @@ public static class Moogle
       public static SearchResult Query(string query) 
       {             
       
-      Vector QueryVector = new Vector(LoadDocuments.ValueDictionary( LoadDocuments.Normalize(query)));
+      Vector QueryVector = new Vector(LoadDocuments.QueryTFIDF(query));
       
       List <float> Scores = Matrix.CalcularSimilitudesCoseno(QueryVector,DocumentMatrix);            
       
 
      
-      SearchItem[] items =  SearchItem.SearchItemsList(Scores,query).ToArray();
+      SearchItem[] items =  SearchItem.SortByScoreDescending(SearchItem.SearchItemsList(Scores,query)).ToArray();
                    
         return new SearchResult(items, query);
     }
